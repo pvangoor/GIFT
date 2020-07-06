a46280dd337df66ed613a254f90a3c66ff62b275
Pieter van Goor <pietervangoor@anu.edu.au>
2020-07-06 11:32:56 +1000

    Implement left exponential

--- GIFT/src/ParameterGroup.cpp
+++ GIFT/src/ParameterGroup.cpp
@@ -16,6 +16,7 @@
 */
 
 #include "ParameterGroup.h"
+#include "eigen3/unsupported/Eigen/MatrixFunctions"
 
 Matrix<ftype, 2, Dynamic> Affine2Group::actionJacobian(const Vector2T& point) const {
     Matrix<ftype, 2, Dynamic> jac;
@@ -26,6 +27,13 @@ Matrix<ftype, 2, Dynamic> Affine2Group::actionJacobian(const Vector2T& point) co
 
 void Affine2Group::applyStepLeft(const VectorXT& step) {
     // Put the step in matrix form, exponentiate, and apply the result.
+    Matrix3T stepMat = Matrix3T::Identity();
+    stepMat(0,0) = step(0); stepMat(0,1) = step(1); stepMat(0,2) = step(4);
+    stepMat(1,0) = step(2); stepMat(1,1) = step(3); stepMat(1,2) = step(5);
+     
+    stepMat = stepMat.exp();
+    this->transformation = stepMat.block<2,2>(0,0) * this->transformation;
+    this->translation = stepMat.block<2,2>(0,0) * this->translation + stepMat.block<2,1>(0,2);
 }
 
 Affine2Group Affine2Group::Identity() {
